da7c1b527d9fd551b44f49440b0d1ac55854bebe
GracjanMWitos <gracjan-witos@wp.pl>
2024-05-19 23:29:19 +0200

    hwRGGASR

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+[System.Serializable]
+public class LevelPaths
+{
+    public string easyPath;
+    public string hardPath;
+}
 
 public class LevelManager : MonoBehaviour
 {
+    public List<LevelPaths> levelPaths = new List<LevelPaths>();
   public void StartGame()
     {
         SceneManager.LoadScene(1);
